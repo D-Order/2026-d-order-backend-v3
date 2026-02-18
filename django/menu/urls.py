82b35d60226294022342c598b75470f800e6daa5
sunwoo030616 <sunwoo030616@dgu.ac.kr>
2026-02-19 01:36:52 +0900

    feat: 사용자 메뉴판 조회 API 및 테스트 통합, 파라미터/응답/유효성검사 일원화

--- django/menu/urls.py
+++ django/menu/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MenuAPIView, MenuDetailAPIView, SetMenuAPIView, SetMenuDetailAPIView
+from .views import MenuAPIView, MenuDetailAPIView, SetMenuAPIView, SetMenuDetailAPIView, BoothMenuListAPIView, UserMenuListAPIView
 
 urlpatterns = [
     # Menu CRUD
@@ -9,4 +9,8 @@ urlpatterns = [
     # SetMenu CRUD
     path('sets/', SetMenuAPIView.as_view(), name='setmenu-create'),
     path('sets/<int:set_id>/', SetMenuDetailAPIView.as_view(), name='setmenu-detail'),
+    
+    # 전체 메뉴판 조회
+    path('menu-list/', BoothMenuListAPIView.as_view(), name='booth-menu-list'),
+    path('<int:booth_id>/menu-list/', UserMenuListAPIView.as_view()),
 ]
