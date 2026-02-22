1558c38a063c7bd4041835d1be73e349a04276ff
nicerjs23 <nicerjs23@gmail.com>
2026-02-23 01:10:33 +0900

    Feat: spring redis pub/sub연동기능 개발

---
+++ spring/src/main/java/com/example/spring/redis/RedisSubscriber.java
@@ -0,0 +1,24 @@
+package com.example.spring.redis;
+
+import com.example.spring.event.RedisMessageEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisSubscriber implements MessageListener {
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    public RedisSubscriber(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String publishedMessage = new String(message.getBody());
+        String channel = new String(message.getChannel());
+        eventPublisher.publishEvent(new RedisMessageEvent(channel, publishedMessage));
+    }
+}
