34508d0664a41a4e5f5ad82325c2b3479c01b4fc
nicerjs23 <nicerjs23@gmail.com>
2026-02-28 21:57:12 +0900

    Chore: 프록시설정, 컨트롤러설정 수정

--- spring/src/main/java/com/example/spring/controller/HealthController.java
+++ spring/src/main/java/com/example/spring/controller/HealthController.java
@@ -12,6 +12,7 @@ import java.util.Map;
  * 헬스체크 컨트롤러
  * Docker/Nginx에서 서비스 상태 확인용
  * Django와 통일된 /health 엔드포인트 사용
+ * nginx/dev.conf설정에서 헬스체크는 예외로 /helth/spring으로 되어 있음 포스트맨 테스트시에는 /helth/spring으로 접근하면됨
  */
 @RestController
 public class HealthController {
