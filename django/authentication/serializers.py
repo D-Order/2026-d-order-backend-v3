b4c9439ce462a7ed38156c2f7cd568bd1f11bf79
chaeunho <chaeunho2002@gmail.com>
2026-03-04 23:29:38 +0900

    fix - 회원가입 시 유효성 검사 조건 요구사항에 맞게 수정

--- django/authentication/serializers.py
+++ django/authentication/serializers.py
@@ -5,8 +5,8 @@ class UserBoothSignupSerializer(serializers.Serializer):
     """
     회원가입 요청 검증용 Serializer (검증만 담당, 생성 로직은 AuthService에 있음)
     """
-    username = serializers.CharField(max_length=150)
-    password = serializers.CharField(write_only=True, min_length=8)
+    username = serializers.CharField(min_length=6, max_length=12)
+    password = serializers.CharField(write_only=True, min_length=4, max_length=20)
     booth_data = BoothSerializer(write_only=True)
 
     # create() 메서드 없음 - 비즈니스 로직은 AuthService.signup_user()에서 처리
