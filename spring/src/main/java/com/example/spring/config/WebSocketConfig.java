32160b9b65c931f642dcd7798fa310b7b654fbea
gn00py48 <gn00py48@gmail.com>
2026-03-29 19:45:38 +0900

    FIX : web socket config fix

--- spring/src/main/java/com/example/spring/config/WebSocketConfig.java
+++ spring/src/main/java/com/example/spring/config/WebSocketConfig.java
@@ -18,14 +18,14 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(servingWebSocketHandler, "/ws/serving")
-                // 2. 허용 도메인을 명확하게 지정 (패턴 사용)
+                // 1. 허용 도메인을 명확하게 지정 (패턴 사용)
                 .setAllowedOriginPatterns(
                         "http://localhost:5173",
                         "https://dev.dorder-api.shop",
                         "http://dev.dorder-api.shop",
                         "https://*.dorder-api.shop" // 서브도메인 대비 안전장치
                 )
-                // 3. HTTP 세션 및 쿠키 정보를 웹소켓 핸드셰이크 시점에 유지해주는 인터셉터
+                // 2. HTTP 세션 및 쿠키 정보를 웹소켓 핸드셰이크 시점에 유지해주는 인터셉터
                 .addInterceptors(new HttpSessionHandshakeInterceptor());
     }
 }
